d859957b582627b3b063b33eb8f271d581ca0e1b
bur-lis <byrakova.lis@gmail.com>
2024-11-01 14:54:21 +0300

    fixing a build error

--- vite.config.ts
+++ vite.config.ts
@@ -7,13 +7,13 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
-                register: resolve(__dirname, 'src/pages/register.html'),
+                register: resolve(__dirname, 'src/pages/register/register.html'),
                 authorization: resolve(__dirname, 'src/pages/authorization/authorization.html'),
-                change_password: resolve(__dirname, 'src/pages/change_password.html'),
-                chat: resolve(__dirname, 'src/pages/chat.html'),
-                client_errors: resolve(__dirname, 'src/pages/client_errors.html'),
-                profile: resolve(__dirname, 'src/pages/profile.html'),
-                server_errors: resolve(__dirname, 'src/pages/server_errors.html'),
+                change_password: resolve(__dirname, 'src/pages/change_password/change_password.html'),
+                chat: resolve(__dirname, 'src/pages/chat/chat.html'),
+                client_errors: resolve(__dirname, 'src/pages/client_errors/client_errors.html'),
+                profile: resolve(__dirname, 'src/pages/profile/profile.html'),
+                server_errors: resolve(__dirname, 'src/pages/server_errors/server_errors.html'),
             },
         },
     },server: {
