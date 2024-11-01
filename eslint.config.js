79025604602a42435f59921558ca4d48dbe50c6a
bur-lis <byrakova.lis@gmail.com>
2024-11-01 15:59:08 +0300

    adding spaces

--- eslint.config.js
+++ eslint.config.js
@@ -9,3 +9,4 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
+
--- hendlbars_precompille.ts
+++ hendlbars_precompille.ts
@@ -21,3 +21,4 @@ export function handlebarsPrecompile(): PluginOption {
         },
     };
 }
+
