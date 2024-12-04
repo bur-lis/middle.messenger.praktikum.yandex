6658d3b1bef1439606568a53fd3a88eac36141e4
Posypkina Elena <byrakova.lis@gmail.com>
2024-12-04 16:24:17 +0300

    deleting path in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,7 @@ export default defineConfig({
     build: {
         rollupOptions: {
             input: {
-                main: resolve(__dirname, 'index.html'),
-                register: resolve(__dirname, 'src/pages/register/register.html'),
-                authorization: resolve(__dirname, 'src/pages/authorization/authorization.html'),
-                change_password: resolve(__dirname, 'src/pages/change_password/change_password.html'),
-                chat: resolve(__dirname, 'src/pages/chat/chat.html'),
-                client_errors: resolve(__dirname, 'src/pages/client_errors/client_errors.html'),
-                profile: resolve(__dirname, 'src/pages/profile/profile.html'),
-                server_errors: resolve(__dirname, 'src/pages/server_errors/server_errors.html'),
+                main: resolve(__dirname, 'index.html')
             },
         },
     }, server: {
